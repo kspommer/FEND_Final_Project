395bd339e2593f8d8025689ecc116352a4ad9234
Susan Pommer <kspommer@gmail.com>
2018-09-26 14:40:32 -0700

    Refactored to get pins to display (name is displaying, pin is not as yet)

--- src/Map.js
+++ src/Map.js
@@ -3,6 +3,7 @@
 
 import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
+import Marker from "./Marker.js"
 
 class Map extends Component {
   render() {
@@ -13,9 +14,17 @@ class Map extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
+
+        {this.props.breweries.map(brewery => {
+          return <Marker 
+            lat={brewery.lat}
+            lng={brewery.lng}
+            text={brewery.name}
+            key={brewery.id}
+          />     
+        })}
+
         </GoogleMapReact>
-        
-        <Marker map={this.state.map}/>
 
       </div>
     );
