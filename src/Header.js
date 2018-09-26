a91725576e7cd98294f7f17df5d0241791fc1cba
Susan Pommer <kspommer@gmail.com>
2018-09-25 17:47:39 -0700

    Initial commit of Header program

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+	render() {
+		return (
+			<div className="header">
+				<header>
+          			<h2>Madison Microbreweries</h2>
+          			<h5>Because we all need a drink to celebrate finishing this class, right?</h5>
+        		</header>
+        	</div>
+        )	
+	}
+}
+
+export default Header
