395c2024008ee7c6ec429e03cd22226462e40eed
Susan Pommer <kspommer@gmail.com>
2018-09-25 17:48:00 -0700

    Initial commit of footer component

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Footer extends Component {
+	render() {
+		return (
+			<div className="footer">
+				<h6>© Susan Pommer - Udacity FEND Program - 2018</h6>
+        	</div>
+        )	
+	}
+}
+
+export default Footer
