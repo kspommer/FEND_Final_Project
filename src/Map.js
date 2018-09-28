93c3fedc302f7d18032588eab5ce147306788f08
Susan Pommer <kspommer@gmail.com>
2018-09-27 20:38:47 -0700

    Major refactor for removal of google-map.react

--- src/Map.js
+++ src/Map.js
@@ -2,30 +2,19 @@
 //reference:  https://www.npmjs.com/package/google-map-react
 
 import React, {Component} from 'react';
-import GoogleMapReact from 'google-map-react';
 import Marker from "./Marker.js"
 
 class Map extends Component {
   render() {
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{key: "AIzaSyC91SKF-vOtspqbdEWrGpWEvYcrv1iQyuU"}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-
+      <div style={{ height: '80vh', width: '100%' }}>
         {this.props.breweries.map(brewery => {
-          return <Marker 
-            lat={brewery.lat}
+          return <Marker lat={brewery.lat}
             lng={brewery.lng}
             text={brewery.name}
             key={brewery.id}
           />     
         })}
-
-        </GoogleMapReact>
-
       </div>
     );
   }
