324012d8d96bd28f6089e2b7a209fa60b145e4ef
Susan Pommer <kspommer@gmail.com>
2018-09-30 18:14:55 -0700

    Successfully relocated API call from App.js

--- src/Markers.js
+++ src/Markers.js
@@ -6,11 +6,17 @@
 // https://developers.google.com/maps/documentation/javascript/markers
 
 import React, {Component} from 'react';
+import SquareAPI from './API_Call.js'
 
 class Markers extends Component {
 
+  state = {
+    breweryAPIdata: []
+  }
+
   componentDidMount() {
     this.displayMarkers()
+    this.displayInfoWindows()
   }
 
   // loop through my favorite breweries to display markers
@@ -22,6 +28,23 @@ class Markers extends Component {
         title: brewery.name,
       })
       marker.setMap(this.props.map);
+    })
+  }
+
+  displayInfoWindows = () => {
+    // my authentication and base URL for FourSquare Get Details of a Venue API 
+    const client_id = "ZWIBO3U1HBUWEJEMMOOGJNRPI1NALRWDIMDNTNPIAKMSUSJO"
+    const client_secret = "OYD1TLNRJVVBZTL31KX0TRWG2AINNBJ1GXKNJTXVXCLHKSNF"
+    const v = "20180929"
+    const baseURL = "https://api.foursquare.com/v2/venues/" 
+
+    // loop through breweries array to get venue_id and call API 
+    this.props.breweries.map(brewery => {
+      let brewery_venue = brewery.venue_Id
+      let endPoint = baseURL + brewery_venue
+      let call = endPoint + "?client_id=" + client_id + "&client_secret=" + client_secret + "&v=" + v
+
+      SquareAPI.getVenueDetails(call).then(results => console.log(results))
     })
   }
 
