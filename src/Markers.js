e5a7b2791e7cc48118524640045067a64d933bf9
Susan Pommer <kspommer@gmail.com>
2018-10-02 21:53:10 -0700

    Changes for latest Marker work -- Markers displaying but onclick non-op for infowindow

--- src/Markers.js
+++ src/Markers.js
@@ -8,79 +8,90 @@
 
 import React, {Component} from 'react';
 import SquareAPI from './API_Call.js'
+import InfoWindow from 'react'
 
 class Markers extends Component {
 
-  componentDidMount() {
-    this.displayMarkers()
+  state = {
+    markers: [] // needed?  
   }
 
-  // loop through my favorite breweries to display marker for each
+  displayWindowOnClick(brewery) {
+    // my authentication and base URL for FourSquare Get Details of a Venue API 
+    const client_id = "ZWIBO3U1HBUWEJEMMOOGJNRPI1NALRWDIMDNTNPIAKMSUSJO"
+    const client_secret = "OYD1TLNRJVVBZTL31KX0TRWG2AINNBJ1GXKNJTXVXCLHKSNF"
+    const v = "20180929"
+    const baseURL = "https://api.foursquare.com/v2/venues/"
+
+    // generate URL for API call
+    let brewery_venue = brewery.venue_Id
+    //let brewery_venue = "4fc813c4e4b0ab3d6544b5ee"
+    let endPoint = baseURL + brewery_venue
+    let call = endPoint + "?client_id=" + client_id + "&client_secret=" + client_secret + "&v=" + v
+
+    // make API call for data for info window
+    SquareAPI.getVenueDetails(call).then(results => {
+      const breweryData = results.response.venue; 
+      this.setState(breweryData)
+
+      console.log(breweryData); // this is an object // TESTING - WORKING
+      console.log(breweryData.name)  // TESTING 
+      console.log(breweryData.location.formattedAddress[0]) // TESTING 
+      console.log(breweryData.location.formattedAddress[1]) // TESTING 
+      console.log(breweryData.url)  // TESTING 
+      console.log(breweryData.contact.formattedPhone)  // TESTING 
+      //return(breweryData) ?  I DO NOT THINK IT NEEDS RETURNED AS USED BELOW
+
+    }).catch(error => console.log('FourSquare error:', error))
+
+    // create info window content string
+    // NEEDS CSS FORMATTING - MORE WORK
+    var infoContent = '<div id="content">' + 
+    '<h4 id="name">{this.breweryData.name}</h4></div>' + 
+    '<h4 id="street address">{this.breweryData.location.formattedAddress[0]})</h4>' + 
+    '<h4 id="city address">{this.breweryData.location.formattedAddress[1]}</h4>' + 
+    '<h4 id="phone">{this.breweryData.contact.formattedPhone}</h4>' + 
+    '<h4 id="url">{this.breweryData.url}</h4>' + 
+    '</div>'
+
+    // Display info window
+    var info = new window.google.maps.InfoWindow({
+      content: infoContent
+      infowindow.open(map, marker)
+    });
+  }
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+
+
+
+
+  componentDidMount() {
+    this.displayMarkers(this.props.brewery)
+  }
+
+  // loop through my favorite breweries array to display marker for each
+  // set onclick action for each marker
   displayMarkers = () => {
     this.props.breweries.map(brewery => {
       var marker = new window.google.maps.Marker ({
         position: {lat: brewery.lat, lng: brewery.lng},
         map: this.props.map,
         title: brewery.name,
+        animation: window.google.maps.Animation.DROP,
       })
-
+      // dislay on the mape
       marker.setMap(this.props.map);
-      this.setState(marker)
+      // set state
+      this.setState(marker);
+      // create onclick
+      marker.addListener('click', this.displayWindowOnClick(brewery));
     })
-  }
-
-  // THIS IS THE FUNCTION I AM NOT SURE WHERE BELONGS...
-  // when click on a marker / call API for more data 
-  marker.addListener('click', function() {
-    getInfoWindowData(brewery)
-  }) 
-
-  getInfoWindowData = (brewery) => {
-    // my authentication and base URL for FourSquare Get Details of a Venue API 
-    const client_id = "ZWIBO3U1HBUWEJEMMOOGJNRPI1NALRWDIMDNTNPIAKMSUSJO"
-    const client_secret = "OYD1TLNRJVVBZTL31KX0TRWG2AINNBJ1GXKNJTXVXCLHKSNF"
-    const v = "20180929"
-    const baseURL = "https://api.foursquare.com/v2/venues/" 
-
-    let brewery_venue = brewery.venue_Id
-    //let brewery_venue = "4fc813c4e4b0ab3d6544b5ee"
-    let endPoint = baseURL + brewery_venue
-    let call = endPoint + "?client_id=" + client_id + "&client_secret=" + client_secret + "&v=" + v
-
-    SquareAPI.getVenueDetails(call).then(results => {
-      const breweryData = results.response.venue;
-      console.log(breweryData); // this is an object // TESTING - WORKING 
-    
-      this.setState(breweryData)
-
-      console.log(breweryData.name)  // TESTING 
-      console.log(breweryData.location.formattedAddress[0]) // TESTING 
-      console.log(breweryData.location.formattedAddress[1]) // TESTING 
-      console.log(breweryData.url)  // TESTING 
-      console.log(breweryData.contact.formattedPhone)  // TESTING 
-      // return(breweryData) ?
-    }).catch(error => console.log('FourSquare error:', error))
-
-      // after get data, call the function to display the infowindow
-      this.displayInfoWindow(this.brewery, this.breweryData)
-  }
-
-  displayInfoWindow(brewery, breweryData) {
-    // create infoWindow content string
-    // CANNOT FIGURE OUT WHY + DOES NOT WORK IN THIS ???
-    var infoContent = '<div id="infoWindowContent"><h1 id="name">{this.breweryData.name}</h1></div>' 
-    //+ <h1 id="street address">{this.breweryData.location.formattedAddress[0]})</h1> + 
-    //<h1 id="city address">{this.breweryData.location.formattedAddress[1]}</h1> + 
-    //<h1 id="phone">{this.breweryData.contact.formattedPhone}</h1>
-    //<h1 id="url">{this.breweryData.url}</h1> + 
-    //</div>
-
-    // Display infowindow
-    var infowindow = new window.google.maps.InfoWindow({
-      content: infoContent
-    });
-
   }  
+
 
   render() {
     return (
@@ -88,6 +99,7 @@ class Markers extends Component {
       </div>
     )  
   }
+
 
 }
 
