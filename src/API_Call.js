db9780dd9bef4c5e24fce205b82ae6f00e1108d0
Susan Pommer <kspommer@gmail.com>
2018-10-06 09:51:40 -0700

    Updates to render marker for each brewery

--- src/API_Call.js
+++ src/API_Call.js
@@ -12,13 +12,14 @@ class Helper {
     //  creates string with my authentication credentials
     static myAuth(){
         const myKeys = {
-            client_id:"ZWIBO3U1HBUWEJEMMOOGJNRPI1NALRWDIMDNTNPIAKMSUSJO",
-            client_secret: "OYD1TLNRJVVBZTL31KX0TRWG2AINNBJ1GXKNJTXVXCLHKSNF",
+            //client_id:"ZWIBO3U1HBUWEJEMMOOGJNRPI1NALRWDIMDNTNPIAKMSUSJO",
+            //client_secret: "OYD1TLNRJVVBZTL31KX0TRWG2AINNBJ1GXKNJTXVXCLHKSNF",
+
+            client_id: "JPFAJLT3GCHEWOCLXYNVDL0XXNXQ2525K2KXEBO5IEYB4UOD",
+            client_secret: "NBHHGTR40PTJXCPKQ3BAM4R42YWPIWOUMI13K1CJO1S5EYG1",
+
             v: "20180929"
         };
-
-        //client_id: "JPFAJLT3GCHEWOCLXYNVDL0XXNXQ2525K2KXEBO5IEYB4UOD",
-        //client_secret: "NBHHGTR40PTJXCPKQ3BAM4R42YWPIWOUMI13K1CJO1S5EYG1",
 
         return Object.keys(myKeys)
         .map(key => `${key}=${myKeys[key]}`)
@@ -48,9 +49,10 @@ class Helper {
     		headers: Helper.headers()
     	};
 
-    	return fetch(`${Helper.fourSquareURL()}${endPoint}?${Helper.myAuth()}&${Helper.urlAdds(urlParameters)}`, requestData)
+    	return fetch(
+            `${Helper.fourSquareURL()}${endPoint}?${Helper.myAuth()}&${Helper.urlAdds(urlParameters)}`, requestData)
         //return fetch("https://api.foursquare.com/v2/venues/4d686a320a25b60c55821790?client_id=ZWIBO3U1HBUWEJEMMOOGJNRPI1NALRWDIMDNTNPIAKMSUSJO&client_secret=OYD1TLNRJVVBZTL31KX0TRWG2AINNBJ1GXKNJTXVXCLHKSNF&v=20180929", requestData)
-            .then(res => res.json())
+            .then(res => res.json());
             //.then(res => res.text()) // convert to plain text -DEBUGGING
             //.then(text => console.log(text)) // console.log - DEBUGGING
     }
@@ -62,9 +64,9 @@ export default class SquareAPI {
         return Helper.fetchAPIData("/venues/search", "GET", urlParameters);
     }
 
-    // function to call FourSquare API Get Venue Details
+    // function to call FourSquare API Get Venue Details -- ERROR HERE 
 	static getVenueDetails(VENUE_ID) {
-		return Helper.fetchAPIData("/venues/${VENUE_ID}", "GET");
+		return Helper.fetchAPIData(`/venues/${VENUE_ID}`, "GET");
 	}
 
 }
