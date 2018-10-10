6b800ec84030d597f0c12b471de1c50c914e4a0a
Susan Pommer <kspommer@gmail.com>
2018-10-10 11:29:04 -0700

    Filter working except on initial load and display of list

--- src/SidePanel.js
+++ src/SidePanel.js
@@ -11,39 +11,79 @@ class SidePanel extends Component {
 	// constructor
 	constructor() { 
 		super();		
-		// set initial state of filter string
+		// set initial state
 		this.state = {
 			query: "",
+			filteredVenues: [],
 		};
 	}
 
+	setFilteredVenues() {
+		const filteredVenues = this.props.venues; 
+		console.log({filteredVenues});
+	}
+
+	//componentWillMount() {
+		//console.log(this.props.venues)
+		//const filteredVenues = this.props.venues; 
+		//console.log({filteredVenues});
+	//}
+
+	// this function is used to filter the venue list displayed based on user input
+	getFilteredList = (query) => {
+		// when user enters input, filter venues array and return reduced array
+		const filteredVenues = this.props.venues.filter(venue =>
+			venue.name.toLowerCase().includes(query.toLowerCase()),
+		);
+		console.log({filteredVenues}) // TESTING 
+		return filteredVenues;
+	};		
+
 	// function to filter markers on user entry in input box
 	filterOnUserEntry = event => {
-		// setState to filter string
-		this.setState({ query: event.target.value });
+		// setState to user-entered filter string
+		const query = event.target.value; 
+		this.setState({query})
+		console.log({query}) // TESTING - WORKS
+
 		// take no action if no user entry 
-		if (event.target.value === "") {
+		if (query === "") {
+			//const filteredVenues = this.props.venues;
+			//console.log({filteredVenues}); 	
 			return
 		}
-		// map over the array of venues to find venue names which match query string
-		const markers = this.props.venues.map(venue => {
-			const match = venue.name.toLowerCase().includes(event.target.value.toLowerCase());
-			// if a query match, id which marker 
-			const marker = this.props.breweryMarkers.find(marker => marker.markerId === venue.id);
-			// change marker's isOpen attribute based on the query match/not a match
-			if(match) {
-				marker.isOpen = true;
-			}
-			else {
-				marker.isOpen = false;
-			}
-			return marker;
-		});
+
+		// if user enters filter query... 
+		else {
+			// pass query to function to filter list
+			const filteredVenues = this.getFilteredList(query);
+			console.log({filteredVenues}) // TESTING 
+			this.setState({filteredVenues})
+
+			// map over the array of venues to find venue names which match query string
+			const markers = this.props.venues.map(venue => {
+				const match = venue.name.toLowerCase().includes(event.target.value.toLowerCase());
+				// if a query match, id which marker 
+				const marker = this.props.breweryMarkers.find(marker => marker.markerId === venue.id);
+				// change marker's isOpen attribute based on the query match/not a match
+				if(match) {
+					marker.isOpen = true;
+				}
+				else {
+					marker.isOpen = false;
+				}
+				return marker;
+			});
 		// call this function (in App.js) to update set of markers (changed isOpen attribute)
 		this.props.updateSuperState({markers});
+		//this.props.updateSuperState({filteredVenues});
+		}
 	}
 
 	render() {
+
+		this.setFilteredVenues(); 
+
     	return (
     		<div className="sidepanel">
 	          	<input type="search" 
@@ -51,9 +91,8 @@ class SidePanel extends Component {
 	          	placeholder="Find a brewery" 
 	          	onChange={this.filterOnUserEntry}
 	          	/>
-
 	    		<ol className="breweryList">
-	    			{this.props.venues && this.props.venues.map(venue  =>
+	    			{this.state.filteredVenues && this.state.filteredVenues.map(venue  =>
 			    		<li 
 			    			className="breweryName" 
 			    			key={venue.id}
