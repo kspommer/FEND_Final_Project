b09f4e17a8430c8b7960b736b134aeca022081ff
Susan Pommer <kspommer@gmail.com>
2018-10-13 11:19:55 -0700

    Refactored to move multiple functions and setStates from sidepanel.js to App.js for better state management

--- src/SidePanel.js
+++ src/SidePanel.js
@@ -8,22 +8,11 @@ import React, {Component} from 'react'
 
 class SidePanel extends Component {
 
-	// constructor
-	constructor() { 
-		super();		
-		// set initial state
-		this.state = {
-			query: "",
-			filteredVenues: [],
-		};
-	}
+	//setFilteredVenues() {
+		//const filteredVenues = this.props.venues; 
+		//console.log({filteredVenues});
+	//}
 
-	setFilteredVenues() {
-		const filteredVenues = this.props.venues; 
-		console.log({filteredVenues});
-	}
-
-	// this went into an infinite loop -- need to understand getDerivedStateFromProps
 	//componentWillMount() {
 		//console.log(this.props.venues)
 		//const filteredVenues = this.props.venues; 
@@ -31,69 +20,76 @@ class SidePanel extends Component {
 	//}
 
 	// this function is used to filter the venue list displayed based on user input
-	getFilteredList = (query) => {
+	//getFilteredList = (query) => {
 		// when user enters input, filter venues array and return reduced array
-		const filteredVenues = this.props.venues.filter(venue =>
-			venue.name.toLowerCase().includes(query.toLowerCase()),
-		);
+		//const filteredVenues = this.props.venues.filter(venue =>
+			//venue.name.toLowerCase().includes(query.toLowerCase()),
+		//);
 		//console.log({filteredVenues}) // TESTING 
-		return filteredVenues;
-	};		
+		//return filteredVenues;
+	//};		
 
 	// function to filter markers on user entry in input box
-	filterOnUserEntry = event => {
+	//filterOnUserEntry = event => {
 		// setState to user-entered filter string
-		const query = event.target.value; 
-		this.setState({query})
-		//console.log({query}) // TESTING
+		//const query = event.target.value; 
+		//this.setState({query})
+		//console.log({query}) // TESTING - WORKS
 
 		// take no action if no user entry 
-		if (query === "") { 	
+		//if (query === "") {
+			//const filteredVenues = this.props.venues;
+			//console.log({filteredVenues}); 	
 			return
-		}
+		//}
+
 		// if user enters filter query... 
-		else {
+		//else {
 			// pass query to function to filter list
-			const filteredVenues = this.getFilteredList(query);
+			//const filteredVenues = this.getFilteredList(query);
 			//console.log({filteredVenues}) // TESTING 
-			this.setState({filteredVenues})
+			//this.setState({filteredVenues})
 
 			// map over the array of venues to find venue names which match query string
-			const markers = this.props.venues.map(venue => {
-				const match = venue.name.toLowerCase().includes(event.target.value.toLowerCase());
+			//const markers = this.props.venues.map(venue => {
+				//const match = venue.name.toLowerCase().includes(event.target.value.toLowerCase());
 				// if a query match, id which marker 
-				const marker = this.props.breweryMarkers.find(marker => marker.markerId === venue.id);
+				//const marker = this.props.breweryMarkers.find(marker => marker.markerId === venue.id);
 				// change marker's isOpen attribute based on the query match/not a match
-				if(match) {
-					marker.isOpen = true;
-				}
-				else {
-					marker.isOpen = false;
-				}
-				return marker;
-			});
+				//if(match) {
+					//marker.isOpen = true;
+				//}
+				//else {
+					//marker.isOpen = false;
+				//}
+				//return marker;
+			//});
 		// call this function (in App.js) to update set of markers (changed isOpen attribute)
-		this.props.updateSuperState({markers});
-		}
+		//this.props.updateSuperState({markers});
+		//}
+	//}
+
+	// when props change, get the new data and trigger render update
+	componentWillReceiveProps = (props) => {
+		this.props = props
 	}
 
 	render() {
 
-		const list = this.getFilteredList(this.state.query); 
-
+		//const list = this.getFilteredList(this.state.query); 
     	return (
     		<div className="sidepanel">
 	          	<input type="search" 
 	          	className="search-brewery" 
 	          	placeholder="Find a brewery" 
-	          	onChange={this.filterOnUserEntry}
+	          	onChange={this.props.filterOnUserEntry}
 	          	/>
 	    		<ol className="breweryList">
-	    			{list && list.map(venue  =>
+	    			{this.props.filteredVenues && this.props.filteredVenues.map(venue  =>
 			    		<li 
 			    			className="breweryName" 
 			    			key={venue.id}
-			    			onClick={() => this.props.openInfoWindowOnClick({...venue})}>
+			    			onClick={() => this.props.openInfoWindowOnClick({venue})}>
 			    			<img src={venue.categories[0].icon.prefix + "32"+ venue.categories[0].icon.suffix} alt={venue.categories[0].name}/>
 			    			{venue.name}
 			    		</li>
