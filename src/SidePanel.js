feabcc88ae5d4726d870b366bde5978a22bd12f3
Susan Pommer <kspommer@gmail.com>
2018-10-13 11:40:33 -0700

    Removed code moved to App.js

--- src/SidePanel.js
+++ src/SidePanel.js
@@ -19,56 +19,6 @@ class SidePanel extends Component {
 		//console.log({filteredVenues});
 	//}
 
-	// this function is used to filter the venue list displayed based on user input
-	//getFilteredList = (query) => {
-		// when user enters input, filter venues array and return reduced array
-		//const filteredVenues = this.props.venues.filter(venue =>
-			//venue.name.toLowerCase().includes(query.toLowerCase()),
-		//);
-		//console.log({filteredVenues}) // TESTING 
-		//return filteredVenues;
-	//};		
-
-	// function to filter markers on user entry in input box
-	//filterOnUserEntry = event => {
-		// setState to user-entered filter string
-		//const query = event.target.value; 
-		//this.setState({query})
-		//console.log({query}) // TESTING - WORKS
-
-		// take no action if no user entry 
-		//if (query === "") {
-			//const filteredVenues = this.props.venues;
-			//console.log({filteredVenues}); 	
-			return
-		//}
-
-		// if user enters filter query... 
-		//else {
-			// pass query to function to filter list
-			//const filteredVenues = this.getFilteredList(query);
-			//console.log({filteredVenues}) // TESTING 
-			//this.setState({filteredVenues})
-
-			// map over the array of venues to find venue names which match query string
-			//const markers = this.props.venues.map(venue => {
-				//const match = venue.name.toLowerCase().includes(event.target.value.toLowerCase());
-				// if a query match, id which marker 
-				//const marker = this.props.breweryMarkers.find(marker => marker.markerId === venue.id);
-				// change marker's isOpen attribute based on the query match/not a match
-				//if(match) {
-					//marker.isOpen = true;
-				//}
-				//else {
-					//marker.isOpen = false;
-				//}
-				//return marker;
-			//});
-		// call this function (in App.js) to update set of markers (changed isOpen attribute)
-		//this.props.updateSuperState({markers});
-		//}
-	//}
-
 	// when props change, get the new data and trigger render update
 	componentWillReceiveProps = (props) => {
 		this.props = props
@@ -98,7 +48,7 @@ class SidePanel extends Component {
 
     		</div>
   		)
-	}
+  	}
 }
 
  export default SidePanel
